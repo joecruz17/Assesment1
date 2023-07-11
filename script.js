b3240d865ef2c5889a83f616d76f713cd2f0bab9
joecruz17 <joejcruz17@gmail.com>
2023-07-11 19:30:35 -0400

    helpme

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+const addNumbers = document.querySelector('.plus')
+const minusNumbers = document.querySelector('.minus')
+const counterDisplay = document.querySelector(".Answer")
+const numberBox = document.querySelector("#dispNumb")
+const upANum = document.querySelector(".up")
+const downANum = document.querySelector(".down")
+
+
+
+
+function addCounter(){
+    counterDisplay.innerHTML = numberBox
+    // counter.innerHTML = counter + numberBox
+}
+
+function subtractCounter(){
+    counter.innerHTML = counter - numberBox
+}
+
+// upANum.addEventListener('click', upOne())
+// downANum.addEventListener('click', downOne())
+// addNumbers.addEventListener('click', addCounter())
+// minusNumbers.addEventListener('click', subtractCounter())
+
+
+addNumbers.onclick = addCounter()
+minusNumbers.onclick = subtractCounter()
